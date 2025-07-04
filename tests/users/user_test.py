7010546f9b006da6e6dd94d14fae3234a96fa735
inncent <innokentiykim90@gmail.com>
2025-07-04 23:48:31 +0500

    service_test and user_test moved

--- tests/users/user_test.py
+++ tests/users/user_test.py
@@ -1,13 +1,10 @@
-import requests
-from conf import SERVICE_URL_2
 from src.baseclasses.response import AssertResponse
 from src.pydantic_schemas.user import UserSchema
 
 
 
-def test_get_users_list():
-    response = requests.get(SERVICE_URL_2)
-    test_obj = AssertResponse(response)
+def test_get_users_list(get_users):
+    test_obj = AssertResponse(get_users)
     test_obj.assert_status_code(200)
     test_obj.validate(UserSchema)
 
