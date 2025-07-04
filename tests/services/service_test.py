b89ac297f5a5d8b77eeac9b18ff97d9d365f7cf2
inncent <innokentiykim90@gmail.com>
2025-07-05 00:47:34 +0500

    mark.skip skipif

--- tests/services/service_test.py
+++ tests/services/service_test.py
@@ -1,13 +1,17 @@
 import requests
+import pytest
 from conf import SERVICE_URL_1
 from src.baseclasses.response import AssertResponse
 from src.enums.global_enums import GlobalErrorMessage
 from src.pydantic_schemas.post import PostSchema
 
 
+@pytest.mark.skip(reason="Issue-1234")
 def test_equal():
     assert 1 == 1
 
+
+@pytest.mark.skipif("1 == 0")
 def test_is_not_equal():
     assert 1 != 2, "one is not equal to two"
 
